b50a971b226ae2f63f6eb2853b16c97d32b94085
Adam Russell <adamlr6+pub@gmail.com>
2014-04-16 01:30:09 -0400

    Prefer ScalatraSpec to ScalatraSuite with FunSpec.

--- src/main/g8/src/test/scala/$package$/$servlet_name$Spec.scala
+++ src/main/g8/src/test/scala/$package$/$servlet_name$Spec.scala
@@ -1,10 +1,9 @@
 package $package$
 
 import org.scalatra.test.scalatest._
-import org.scalatest.FunSpec
 
 // For more on ScalaTest, see http://www.scalatest.org/quick_start
-class $servlet_name$Spec extends ScalatraSuite with FunSpec {
+class $servlet_name$Spec extends ScalatraSpec {
   addServlet(classOf[$servlet_name$], "/*")
 
   describe("GET / on $servlet_name$") {
